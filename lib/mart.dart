9689727453297221af3a312058555deb3a1e10f4
ahmedbera <ahmedbera@gmail.com>
2018-10-09 13:24:07 +0300

    implement ajax requests

--- lib/mart.dart
+++ lib/mart.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:math';
+import 'package:meta/meta.dart';
 import 'package:http/http.dart' as http;
 import 'package:mart_vm/parser/mangaParser.dart';
 import 'package:mart_vm/parser/searchResultsParser.dart';
@@ -81,22 +83,83 @@ class Mart {
     });
   }
 
-  static Future updateLists({String id, listId, bool delete, chapter, volume}) async {
-    /** TODO
+  static Future updateLists({
+    @required String id,
+    @required String type,
+    listId,
+    bool delete=false,
+    int chapter,
+    int volume
+  }) async {
+    /** @required params
      * id: manga id
      * cache_j={number},{number},{number} 3, 8 character random number
-     * * * * * * * * * * * * * *
-     * l: list id (0 is delete)
+     * type=["list", "chap"]
+     * * * Update list
+     * l: list id (0 is delete/not in list)
      * r: remove (probably)
      * inc_c: increment chapter (can be negative)
      * inc_v: increment volume (can be negative)
-     * * * * * * * * * * * * * *
+     * * * Set list
      * set_c: set chapter number
      * set_v: set volume number
+     * if r=1 is used also use inc_c=1 and inc_v=1
      */
-    var url_list = 'https://www.mangaupdates.com/ajax/list_update.php?s=${id}&l=0&r=1&inc_c=1&inc_v=1&cache_j=19513573,84101695,22476166';
-    var url_chap = 'https://www.mangaupdates.com/ajax/chap_update.php?s=${id}&set_v=${volume}&set_c=${chapter}&cache_j=65548501,29918347,4231017';
+    String list_update = 'https://www.mangaupdates.com/ajax/list_update.php?s=${id}';
+    String chap_update = 'https://www.mangaupdates.com/ajax/chap_update.php?s=${id}';
+    String list_id = listId == null ? '&l=0' : '';
+    String remove = delete ? '&l=0&r=1' : '';
+    String inc_c = chapter == null ? '&inc_c=${chapter}' : '';
+    String inc_v = volume == null ? '&inc_v=${volume}' : '';
+    String set_c = chapter == null ? '&set_c=${chapter}' : '';
+    String set_v = volume == null ? '&set_v=${volume}' : '';
+    String cache = '&cache_j=';
+
+    String url = '';
+    if(type == "list") {
+      url = list_update;
+      url += list_id;
+      url += remove;
+      url += inc_c;
+      url += inc_v;
+    } else if(type == "chap") {
+      url = chap_update;
+      url += set_c;
+      url += set_v;
+    }
+    // Generate cache
+    Random rnd = new Random();
+    for (var i = 0; i < 26; i++) {
+      if(i == 8 || i == 17) {
+        cache += ',';
+      } else {
+        cache += rnd.nextInt(10).toString();
+      }
+    }
+    
+    url += cache;
+
+    return makeRequest(url).then((res) {
+      return res;
+    });
   }
+  // TODO: write AjaxResponse class
+  static Future chapterIncrement([String id, int increment=1]) async {
+    return updateLists(id: id, chapter: increment, type: "list");
+  }
+  static Future volumeIncrement([String id, int increment=1]) async {
+    return updateLists(id: id, volume: increment, type: "list");
+  }
+  static Future removeManga([String id]) async {
+    return updateLists(id: id, type: "list", delete: true);
+  }
+  static Future chapterUpdate(String id, int chapter, int volume) async {
+    return updateLists(id: id, type: "chap", chapter: chapter, volume: volume);
+  }
+  static Future moveToList(String id, listId) async {
+    // TODO: investigate inc/set params work during list updates
+  }
+
 
   static Future<String> makeRequest(url) async {
     return http.read(url, headers: {
