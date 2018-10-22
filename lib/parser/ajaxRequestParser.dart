e74afcf69291ec15d119ad7b7e4ad2654bac2711
ahmedbera <ahmedbera@gmail.com>
2018-10-22 13:11:10 +0300

    add todo's for ajax requests

---
+++ lib/parser/ajaxRequestParser.dart
@@ -0,0 +1,18 @@
+import 'package:mart_vm/models/listInfo.dart';
+
+// TODO
+/*
+  // only in chapter increment
+  mu id => \d+: // might be not necessary
+  
+  // common in all ajax responses
+  chapter => c.\d+
+    + split by .
+  volumes => v.\d+
+    + split by .
+  
+  // only in change list [tested in addtolist]
+  otherLists => /option value.* /g
+    + foreach => split('>')[1].split('<')[0]
+
+*/
